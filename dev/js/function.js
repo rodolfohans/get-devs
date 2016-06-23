5dc396fc1526fe007fba5a76c04aea4348bbcff8
rodolfohans <rodolfohans@gmail.com>
2016-06-23 01:43:51 -0300

    function filter

--- dev/js/function.js
+++ dev/js/function.js
@@ -1,10 +1,12 @@
 $(document).ready(function inicio() {
-	loadProducts();
-	
+	loadProducts();	
 });
 
 var spots = 9;			//quantidade de produtos exibidos na página
-	prodArr = [];		//array de produtos
+var	prodArr = [];		//array de produtos
+var	$cor = $('.f-cor');
+var	$tamanho = $('.f-tamanho');
+var	$preco = $('.f-preco');
 
 function loadProducts() {
 	var xmlhttp = new XMLHttpRequest();
@@ -24,13 +26,16 @@ function writeProducts(arr) {
     var out = "";
     var i;
 
-    for(i = 0; i < spots; i++) {
+    for(i = 0; i < arr.length; i++) {
         out += '<div class="cont-33 produto-wrapper"><div class="produto-image"><img src="images/produtos/' +
         arr[i].thumbImage + '.jpg"></div></br><div class="produto-nome"><h3>' +
         arr[i].nome + '</h3></br></div><div class="produto-info"><h4>R$ ' +
         arr[i].valor + '</h4><span>até ' +
         arr[i].vezes + 'x de R$' + 
-        arr[i].parcela + '</span></div><button onclick="" class="botao-comprar"><i class="icon-basket"></i></button></div>';
+        arr[i].parcela + ',00</span></div><button onclick="" class="botao-comprar"><i class="icon-basket"></i></button></div>';
+        if(i + 1 == spots){
+        	break;
+        }
     }
     document.getElementById("galeria-produtos").innerHTML = out;
 }
@@ -38,4 +43,60 @@ function writeProducts(arr) {
 function showMore() {
 	spots = spots + 3;
 	writeProducts(prodArr);
+}
+
+$cor.click(function () {
+	onChange();
+});
+
+$tamanho.click(function () {
+	onChange();
+});
+
+$preco.click(function () {
+	onChange();
+});
+
+function onChange() {
+	var findedProducts = findProduct();
+	writeProducts(findedProducts);
+}
+
+function findProduct() {
+	return filter($cor.filter(":checked").val(),$tamanho.filter(":checked").val(),$preco.filter(":checked").val());
+}
+
+function filter(cor, tamanho, preco) {
+	var result = [];
+	//var	found = false;
+
+	for (var i = 0; i < prodArr.length; i++) {
+		var product = prodArr[i];
+		//var found = false;
+		if ((!cor || product.cor === cor) && (!tamanho || product.tamanho === tamanho) && (!preco || sortPrice(product.valor, preco) == true)){
+			result.push(product);
+			/*for (var j = 0; j < product.cor.length; j++) {
+				var pCor = product.cor[j];
+
+				if (cor === pCor) {
+					found = true;
+					break;
+				}
+			}*/
+		}
+		/*if (found) {
+			result.push(product);
+		}*/
+	}
+
+	return result;
+}
+
+function sortPrice(precoProduto, janelaValor) {
+	var a = janelaValor.split("-");
+	var result = false;
+	if ((precoProduto >= Number(a[0])) && (precoProduto <= Number(a[1]))) {
+		result = true;
+	}
+	return result;
 }
